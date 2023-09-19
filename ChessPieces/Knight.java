bd071758dceb7837c289e642eafea4e837482312
mundembe <61733352+mundembe@users.noreply.github.com>
2023-09-19 21:00:48 +0200

    set up Knight navigation skeleton

--- ChessPieces/Knight.java
+++ ChessPieces/Knight.java
@@ -19,6 +19,51 @@ public class Knight extends Piece{
 
     @Override
     public void showPossibleMoves(Square[][] blocks){
+
+        // Far Down
+        if (xCoordinate + 2 < 8){
+            if (yCoordinate + 1 < 8){
+                xPossibleMoves[0] = xCoordinate + 2;
+                yPossibleMoves[0] = yCoordinate + 1;
+            }
+            if (yCoordinate - 1 >= 0){
+                xPossibleMoves[1] = xCoordinate + 2;
+                yPossibleMoves[1] = yCoordinate - 1;
+            }
+        }
+        // Far Up
+        if (xCoordinate - 2 >= 0){
+            if (yCoordinate + 1 < 8){
+                xPossibleMoves[2] = xCoordinate - 2;
+                yPossibleMoves[2] = yCoordinate + 1;
+            }
+            if (yCoordinate - 1 >= 0){
+                xPossibleMoves[3] = xCoordinate - 2;
+                yPossibleMoves[3] = yCoordinate - 1;
+            }
+        }
+        // near Down
+        if (xCoordinate + 1 < 8){
+            if (yCoordinate + 2 < 8){
+                xPossibleMoves[4] = xCoordinate + 1;
+                yPossibleMoves[4] = yCoordinate + 2;
+            }
+            if (yCoordinate - 2 >= 0){
+                xPossibleMoves[5] = xCoordinate + 1;
+                yPossibleMoves[5] = yCoordinate - 2;
+            }
+        }
+        // Near Up
+        if (xCoordinate - 1 >= 0){
+            if (yCoordinate + 2 < 8){
+                xPossibleMoves[6] = xCoordinate - 1;
+                yPossibleMoves[6] = yCoordinate + 2;
+            }
+            if (yCoordinate - 2 >= 0){
+                xPossibleMoves[7] = xCoordinate - 1;
+                yPossibleMoves[7] = yCoordinate - 2;
+            }
+        }
         
     }
 
